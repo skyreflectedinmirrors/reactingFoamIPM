72796273db559c2cb46ab802d766cdd1a0c612f8
Nick <nicholas.curtis@uconn.edu>
2018-10-30 17:09:47 -0400

    update

--- example_build.sh
+++ example_build.sh
@@ -1,1 +1,1 @@
-WRAPPER_PATH=`pwd`/BatchedODE/AccelerInt/ CL_INC_DIR=/opt/opencl-headers/ PYJAC_PATH=`pwd`/pyjac/ PYJAC_LIB_PATH=`pwd`/lib/ ACCELERINT_PATH=`pwd`/include ACCELERINT_LIB_PATH=`pwd`/lib/ ./Allwmake -j12
+WRAPPER_PATH=`pwd`/BatchedODE/AccelerInt/ CL_INC_DIR=/opt/opencl-headers/ PYJAC_PATH=`pwd`/include/ PYJAC_LIB_PATH=`pwd`/lib/ ACCELERINT_PATH=`pwd`/include ACCELERINT_LIB_PATH=`pwd`/lib/ ./Allwmake -j12
