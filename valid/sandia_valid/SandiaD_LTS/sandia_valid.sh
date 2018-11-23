096aaea8e4aa1b9bd6abd410e19da9d5684c9830
Nick <nicholas.curtis@uconn.edu>
2018-11-23 13:38:00 -0500

    update

--- valid/sandia_valid/SandiaD_LTS/sandia_valid.sh
+++ valid/sandia_valid/SandiaD_LTS/sandia_valid.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=serial           # run on serial partition
-#SBATCH --time=4-00:00:00            # Job should run for no more than 4 days
+#SBATCH --time=7-00:00:00            # Job should run for no more than 7 days
 #SBATCH --nodes=1                    # two nodes
 #SBATCH --ntasks=10                  # 10 cores
 #SBATCH --exclude=cn[01-136,325-328] # only run on haswell
@@ -11,5 +11,4 @@
 #SBATCH --mail-user=nicholas.curtis@uconn.edu
 #SBATCH --dependency=singleton
 
-
-mpirun reactingFoam -case . -parallel
+./Allrun
