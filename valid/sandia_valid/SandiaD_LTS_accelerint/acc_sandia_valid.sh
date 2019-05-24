d3a69f3ed776291b9ea34df7b801a99bdd51c761
Nick <nicholas.curtis@uconn.edu>
2019-05-24 08:51:50 -0400

    update haswell nodes

--- valid/sandia_valid/SandiaD_LTS_accelerint/acc_sandia_valid.sh
+++ valid/sandia_valid/SandiaD_LTS_accelerint/acc_sandia_valid.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=7-00:00:00            # Job should run for no more than 7 days
 #SBATCH --nodes=1                    # two nodes
 #SBATCH --ntasks=10                  # 10 cores
-#SBATCH --exclude=cn[01-136,325-328] # only run on haswell
+#SBATCH --exclude=cn[65-69,71-136,325-343,345-353,355-358,360-364,369-398,400-401],gpu[07-10] # only run on haswell
 #SBATCH --mem 100G                   # up to 100 gigs
 #SBATCH -o les_nonreacting.out
 #SBATCH -e les_nonreacting.out
